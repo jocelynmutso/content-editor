e49ba67b4449e9917c05a17b33ef9e8cdea1bc90
Jocelyn Mutso (ext) <jocelyn.mutso@resys.io>
2021-07-24 12:03:41 +0300

    context + composerselect

--- src/core/composers/release/ReleaseComposer.tsx
+++ src/core/composers/release/ReleaseComposer.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import { makeStyles, createStyles, Theme, TextField, Typography } from '@material-ui/core';
-import { API } from '../../deps';
+import {
+  makeStyles, createStyles, Theme, TextField, InputLabel, FormControl, MenuItem, Select,
+  Button, Dialog, Typography, DialogTitle, DialogContent, DialogActions,
+} from '@material-ui/core';
+
+import { API, Ide } from '../../deps';
 
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -15,21 +19,44 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
-interface ReleaseComposerProps {
-  site: API.CMS.Site,
-  releases: API.CMS.Releases,
-}
-
-
-const ReleaseComposer: React.FC<ReleaseComposerProps> = ({ site, releases }) => {
+const ReleaseComposer: React.FC<{ onClose: () => void }> = ({ onClose }) => {
   const classes = useStyles();
+  const ide = Ide.useIde();
 
+  const [name, setName] = React.useState('');
+  const [note, setNote] = React.useState('');
+
+  const handleCreate = () => {
+    const entity: API.CMS.CreateRelease = { name, note };
+    ide.service.create().release(entity).then(success => {
+      console.log(success)
+      onClose();
+      ide.actions.handleLoadSite();
+    })
+  }
   return (
-    <Typography className={classes.heading}>
-      <TextField className={classes.formControl} id="outlined-basic" label="Tag" variant="outlined" fullWidth />
-      <TextField className={classes.formControl} id="outlined-basic" label="Note" variant="outlined" fullWidth />
-      <TextField className={classes.formControl} disabled id="outlined-basic" label="Date" defaultValue={new Date().toISOString()} variant="outlined" fullWidth />
-    </Typography>
+    <Dialog open={true} onClose={onClose} >
+      <DialogTitle>Create a new release</DialogTitle>
+      <DialogContent>
+
+        <Typography className={classes.heading}>
+          <TextField className={classes.formControl}
+            fullWidth
+            id="outlined-basic" label="Tag Name" variant="outlined"
+            onChange={({ target }) => setName(target.value as any)}
+          />
+          <TextField className={classes.formControl}
+            fullWidth
+            id="outlined-basic" label="Note" variant="outlined"
+            onChange={({ target }) => setNote(target.value as any)} />
+          <TextField className={classes.formControl} disabled id="outlined-basic" label="Date" defaultValue={new Date().toISOString()} variant="outlined" fullWidth />
+        </Typography>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose} color="inherit">Cancel</Button>
+        <Button onClick={handleCreate} color="primary" autoFocus disabled={!name}>Create</Button>
+      </DialogActions>
+    </Dialog>
   );
 }
 
--- src/core/composers/workflow/WorkflowComposer.tsx
+++ src/core/composers/workflow/WorkflowComposer.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { makeStyles, createStyles, Theme, InputLabel, FormControl, TextField } from '@material-ui/core';
-import Typography from '@material-ui/core/Typography';
-import MenuItem from '@material-ui/core/MenuItem';
-import Select from '@material-ui/core/Select';
-
-import { API } from '../../deps';
+import {
+  makeStyles, createStyles, Theme, TextField, InputLabel, FormControl, MenuItem, Select,
+  Button, Dialog, Typography, DialogTitle, DialogContent, DialogActions,
+} from '@material-ui/core';
+import { API, Ide } from '../../deps';
 
 
 
@@ -20,53 +19,76 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
-interface WorkflowComposerProps {
-  site: API.CMS.Site,
-}
 
-
-const WorkflowComposer: React.FC<WorkflowComposerProps> = ({ site }) => {
+const WorkflowComposer: React.FC<{ onClose: () => void }> = ({ onClose }) => {
   const classes = useStyles();
+  const ide = Ide.useIde();
+  const { site } = ide.session;
+
   const [locale, setLocale] = React.useState('');
-  const [workflow, setWorkflow] = React.useState('');
+  const [content, setContent] = React.useState('');
+  const [name, setName] = React.useState('');
+
+
+  const handleCreate = () => {
+    const entity: API.CMS.CreateWorkflow = { content, locale, name };
+    ide.service.create().workflow(entity).then(success => {
+      console.log(success)
+      onClose();
+      ide.actions.handleLoadSite();
+    })
+  }
 
 
   const workflows: API.CMS.Workflow[] = Object.values(site.workflows);
   const locales: API.CMS.SiteLocale[] = Object.values(site.locales);
 
   return (
-    <Typography className={classes.heading}>
-      <FormControl variant="outlined" className={classes.select} fullWidth>
-        <InputLabel>Locale</InputLabel>
-        <Select
-          onChange={({ target }) => setLocale(target.value as any)}
-          value={locale}
-          label="Locale"
-        >
-          {locales.map((locale, index) => (
-            <MenuItem key={index} value={locale.value}>{locale.value}</MenuItem>
-          ))}
-        </Select>
+    <Dialog open={true} onClose={onClose} >
+      <DialogTitle>Add a workflow</DialogTitle>
+      <DialogContent>
 
-      </FormControl>
-      <FormControl variant="outlined" className={classes.select} fullWidth>
-        <InputLabel>Workflow</InputLabel>
-        <Select
-          onChange={({ target }) => setWorkflow(target.value as any)}
-          value={workflow}
-          label="workflow"
-        >
-          {workflows.map((workflow, index) => (
-            <MenuItem key={index} value={workflow.name}>{workflow.name}</MenuItem>
-          ))}
-        </Select>
+        <Typography className={classes.heading}>
+          <FormControl variant="outlined" className={classes.select} fullWidth>
+            <InputLabel>Locale</InputLabel>
+            <Select
+              onChange={({ target }) => setLocale(target.value as any)}
+              value={locale}
+              label="Locale"
+            >
+              {locales.map((locale, index) => (
+                <MenuItem key={index} value={locale.value}>{locale.value}</MenuItem>
+              ))}
+            </Select>
 
-      </FormControl>
-      <TextField className={classes.select} label="Localised Name" variant="outlined" placeholder="Permit application form" helperText="Name to be displayed to users" fullWidth />
+          </FormControl>
+          <FormControl variant="outlined" className={classes.select} fullWidth>
+            <InputLabel>Content</InputLabel>
+            <Select
+              onChange={({ target }) => setContent(target.value as any)}
+              value={content}
+              label="workflow"
+            >
+              {workflows.map((workflow, index) => (
+                <MenuItem key={index} value={workflow.name}>{workflow.name}</MenuItem>
+              ))}
+            </Select>
 
-    </Typography>
+          </FormControl>
+          <TextField className={classes.select}
+            label="Localised Name" variant="outlined" placeholder="Permit application form" helperText="Name to be displayed to users" fullWidth
+            value={name}
+            onChange={({ target }) => setName(target.value)} />
+        </Typography>
 
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose} color="inherit">Cancel</Button>
+        <Button onClick={handleCreate} color="primary" autoFocus disabled={!name}>Add</Button>
+      </DialogActions>
+    </Dialog>
   );
+
 }
 
 export { WorkflowComposer }
